035d199cdb6d8e3c165c298ebfab9a8fee815851
Abu Salah Musha Lemon <lemonahmed512@gmail.com>
2025-09-07 22:23:12 +0600

    upload dashboard

--- ERP/script.js
+++ ERP/script.js
@@ -1,31 +1,39 @@
+    $(document).ready(function () {
+      const $sidebar = $('#sidebar');
+      const $overlay = $('#sidebarOverlay');
 
+      const isMobile = () => window.innerWidth <= 768;
 
-$(document).ready(function () {
-  const isMobile = () => window.innerWidth <= 768;
+      function toggleSidebar() {
+        if (isMobile()) {
+          $sidebar.toggleClass('mobile-show');
+          $overlay.toggleClass('mobile-show');
+        } else {
+          $('body').toggleClass('sidebar-collapsed');
+        }
+      }
 
-  function toggleSidebar() {
-    const $sidebar = $('#sidebar');
+      $('#toggleSidebar').click(toggleSidebar);
 
-    if (isMobile()) {
-      $sidebar.toggleClass('show');
-    } else {
-      $sidebar.toggleClass('collapsed');
-      $('body').toggleClass('sidebar-closed');
-    }
-  }
+      $overlay.click(function () {
+        $sidebar.removeClass('mobile-show');
+        $overlay.removeClass('mobile-show');
+      });
 
-  // Toggle sidebar
-  $('#toggleSidebar').click(toggleSidebar);
+      toastr.options = {
+        "closeButton": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "timeOut": "3000"
+      };
 
-  // Toastr notification
-  toastr.options = {
-    "closeButton": true,
-    "progressBar": true,
-    "positionClass": "toast-top-right",
-    "timeOut": "3000"
-  };
+      $('#showToast').click(function () {
+        toastr.success("Payroll processed successfully!", "Success");
+      });
 
-  $('#showToast').click(function () {
-    toastr.success("Payroll processed successfully!", "Success");
-  });
-});
+      $('#toggleDarkMode').click(function () {
+        $('body').toggleClass('dark-mode');
+        $(this).find('i').toggleClass('fa-moon fa-sun');
+        toastr.info($('body').hasClass('dark-mode') ? "Dark mode enabled" : "Light mode enabled");
+      });
+    });
